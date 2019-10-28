d43b70ffeb7f07e3a64f813e0474a679d71c795a
Pere <pere.solaclaver@gmail.com>
2019-10-28 13:06:56 +0000

    Complete steps and tested catchall endpoint with postman

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ const cors = require('cors');
 
 const server = express();
 
+server.use(cors());
+server.use(express.json());
+
+server.get('*', handleDefaultRequest)
+
+function handleDefaultRequest(req, res) {
+    res.json('hello from node')
+}
+
+server.listen(process.env.PORT || 3000, () => {
+    console.log('listening on ' + (process.env.PORT || 3000))
+});
