53b117c6b0c0d571820829698ae83430a344335f
Pere <pere.solaclaver@gmail.com>
2019-10-28 14:42:11 +0000

    Get user by id with fall back error in case server is nto available or user is not found

--- index.js
+++ index.js
@@ -1,33 +1,55 @@
 // implement your API here
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const db = require('./data/db');
+const db = require("./data/db");
 
 const server = express();
 
 server.use(cors());
 server.use(express.json());
 
-server.get('/api/users', getAllUsers)
-server.get('*', handleDefaultRequest)
+server.get("/api/users/:id", getUserById);
+server.get("/api/users", getAllUsers);
+server.get("*", handleDefaultRequest);
 
-function getAllUsers(req, res) {
-    db.find()
+function getUserById(req, res) {
+  const { id } = req.params;
+  db.findById(id)
     .then(data => {
-        console.log(data)
-        res.json(data)
+      console.log(data);
+      !data
+        ? res
+            .status(404)
+            .json({ message: "The user with the specified ID does not exist." })
+        : res.json(data);
     })
     .catch(error => {
-        res.status(500).json({ error: "The users information could not be retrieved." })
-        console.log(error)
+      console.log(error);
+      res
+        .status(500)
+        .json({ error: "The user information could not be retrieved." });
+    });
+}
+
+function getAllUsers(req, res) {
+  db.find()
+    .then(data => {
+      console.log(data);
+      res.json(data);
     })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ error: "The users information could not be retrieved." });
+      console.log(error);
+    });
 }
 
 function handleDefaultRequest(req, res) {
-    res.json('hello from node')
+  res.json("hello from node");
 }
 
 server.listen(process.env.PORT || 3000, () => {
-    console.log('listening on ' + (process.env.PORT || 3000))
+  console.log("listening on " + (process.env.PORT || 3000));
 });
