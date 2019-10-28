987cdaf6485bc86eb72246205f6b5c1c3426b1df
Pere <pere.solaclaver@gmail.com>
2019-10-28 13:36:09 +0000

    get all users endpoit working

--- index.js
+++ index.js
@@ -2,12 +2,26 @@
 const express = require('express');
 const cors = require('cors');
 
+const db = require('./data/db');
+
 const server = express();
 
 server.use(cors());
 server.use(express.json());
 
+server.get('/api/users', getAllUsers)
 server.get('*', handleDefaultRequest)
+
+function getAllUsers(req, res) {
+    db.find()
+    .then(data => {
+        console.log(data)
+        res.json(data)
+    })
+    .catch(error => {
+        console.log(error)
+    })
+}
 
 function handleDefaultRequest(req, res) {
     res.json('hello from node')
